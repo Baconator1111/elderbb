1c22127a30fb3b16f029a972a33d4a48ebf39129
Ben Johnson <holloo111@gmail.com>
2018-01-19 15:57:14 -0700

    product list is done

--- elderbb/elderbb-react-app/server/controllers/products.js
+++ elderbb/elderbb-react-app/server/controllers/products.js
@@ -1,25 +1,58 @@
 const productList = {
     white: [{
         product: 'White .20g',
-        amount: '1kg 5000 count'
+        amount: '1kg 5000 count',
+        price: '12.00'
     },{
         product: 'White .25g',
-        amount: '1kg 4000 count'
+        amount: '1kg 4000 count',
+        price: '13.50'
     },{
         product: 'White .30g',
-        amount: '1kg 3350 count'
+        amount: '1kg 3350 count',
+        price: '15.00'
     },{
         product: 'White .32g',
-        amount: '1kg 3125 count'
+        amount: '1kg 3125 count',
+        price: '17.00'
     },{
         product: 'White .36g',
-        amount: '1kg 2750 count'
+        amount: '1kg 2750 count',
+        price: '20.00'
     },{
         product: 'White .40g',
-        amount: '1kg 2500 count'
+        amount: '1kg 2500 count',
+        price: '22.00'
     },{
         product: 'White .43g',
-        amount: '1kg 2350 count'
+        amount: '1kg 2350 count',
+        price: '24.00'
+    }],
+    biodegradable: [{
+        product: 'biodegradable .20g',
+        amount: '1kg 5000 count',
+        price: '15.00'
+    },{
+        product: 'biodegradable .25g',
+        amount: '1kg 4000 count',
+        price: '16.50'
+    },{
+        product: 'biodegradable .28g',
+        amount: '1kg 3575 count',
+        price: '17.50'
+    },{
+        product: 'biodegradable .30g',
+        amount: '1kg 3350 count',
+        price: '18.00'
+    },{
+        product: 'biodegradable .25g',
+        amount: '1kg 4000 count',
+        price: '16.50'
+    }],
+    tracer: [{
+        product: 'tracer .25g',
+        amount: '.5kg 2000 count',
+        price: '14.00'
     }]
 }
 
